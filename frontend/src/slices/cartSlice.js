cf6002331bc4265a5d9cedb5ecf869bb39e0cb63
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-05-06 12:12:20 +0300

    Itemcount

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { addDecimals, updateCart } from "../utils/cartUtils";
+import { updateCart } from "../utils/cartUtils";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
