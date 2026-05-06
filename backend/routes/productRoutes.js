85abe3f78f9716fec7f40673bb9deb3c62800fd6
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-05-06 09:19:54 +0300

    controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,29 +1,13 @@
 import express from "express";
-import asyncHandler from "../middleware/asyncHaqndler.js";
-import Product from "./../models/productModel.js";
+import {
+  getProductById,
+  getProducts,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find();
-    res.json(products);
-  }),
-);
+router.get("/", getProducts);
 
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      return res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Resource not found");
-    }
-  }),
-);
+router.get("/:id", getProductById);
 
 export default router;
